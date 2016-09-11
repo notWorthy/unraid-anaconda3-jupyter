1220fe0e245ce1f4eb783c72b9bed11ffd28ad7f
Damiano Lombardo <lombardo.damiano@gmail.com>
2016-09-11 17:01:59 +0800

    corrected repo name in files

--- start.bash
+++ start.bash
@@ -5,7 +5,7 @@ echo "Setting JUPYTER_CONFIG_DIR"
 JUPYTER_CONFIG_DIR="/opt/jupyterprofile"
 export JUPYTER_CONFIG_DIR
 echo "Downloading startup script"
-wget -O opt/jupyterprofile/jupyter_notebook_config.py https://github.com/damianolombardo/unraid-anaconda3-ipython/raw/master/jupyter_notebook_config.py
+wget -O opt/jupyterprofile/jupyter_notebook_config.py https://github.com/damianolombardo/unraid-anaconda3-jupyter/raw/master/jupyter_notebook_config.py
 echo "Starting notebook server"
 /opt/conda/bin/jupyter notebook
 
